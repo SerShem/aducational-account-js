b3d302723bf1a2f07b71ea90d1f729d665ea4138
Serhii Shemendiuk <96024675+SerShem@users.noreply.github.com>
2022-12-18 17:27:24 +0200

    js 4, 5

---
+++ js/test4.js
@@ -0,0 +1,13 @@
+/* const shouldRenew = confirm('do you whant renew signer?');
+console.log(shouldRenew); */
+
+/* const quantity = prompt('enter the quantity of the produck');
+console.log(quantity);
+console.log(typeof quantity);
+ */
+
+/* let quantity = prompt('enter the quantity of the produck');
+quantity = Number(quantity);
+console.log(quantity);
+console.log(typeof quantity); */
+
