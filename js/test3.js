94997ff8ea6df329cc704cf072bb9c48ba24a199
Serhii Shemendiuk <96024675+SerShem@users.noreply.github.com>
2022-12-18 12:29:18 +0200

    js3

---
+++ js/test3.js
@@ -0,0 +1,18 @@
+const age = 10;
+const totalPrise = 200.74;
+const userName = 'Serhii';
+const message = true;
+const shouldConfirm = false;
+
+console.log('Cost:', totalPrise);
+
+console.log('500');
+
+console.log(message);
+
+console.log(typeof (age));
+console.log(typeof (message));
+
+console.log('before');
+alert('some one message');
+console.log('after');
