ce4801e034b97c58b262dcb5409971596c329d52
Serhii Shemendiuk <96024675+SerShem@users.noreply.github.com>
2022-12-18 18:52:50 +0200

    js 5,6.7

---
+++ js/test5.js
@@ -0,0 +1,49 @@
+
+/* let elementWidth = '58.23px'; */
+
+/* elementWidth = Number.parseInt(elementWidth);
+console.log('elementWidth ', elementWidth);
+console.log(elementWidth); */
+
+/* let elementHeigth = '200.74px'; */
+/* elementHeigth = Number.parseInt(elementHeigth);
+console.log('elementHeigth ', elementHeigth);
+console.log(elementHeigth); */
+
+
+
+/* let salary = 1300.16472; */
+/* salary = Number(salary.toFixed(2)); */
+/* salary = Number(salary) */
+/* console.log(salary); */
+/* console.log(Number(salary.toFixed(2))); */
+
+
+
+/* let quantity = '30';
+let value = 'this string cant transfom to number'; */
+/* console.log(value); */
+
+/* console.log(Number(quantity));
+console.log(Number(value)); */
+
+
+/* const base = 2;
+const power = 5; */
+
+/* const result = Math.pow(base, power);
+console.log(result); */
+
+/* let base = prompt('Enter a number');
+base = Number(base);
+console.log(base);
+let power = prompt('Enter a power');
+power = Number(power);
+console.log(power);
+const result = base ** power;
+console.log(result); */
+/* const max = 10;
+const min = 0;
+const result = Math.round(Math.random() * (max - min) + min); 
+console.log(result);  */
+
