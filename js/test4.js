acce0d572cf5c561480528bec297635d53dd6327
Serhii Shemendiuk <96024675+SerShem@users.noreply.github.com>
2023-01-17 22:43:54 +0200

    1

--- js/test4.js
+++ js/test4.js
@@ -5,7 +5,6 @@ const quantity = prompt('enter the quantity of the produck');
 console.log(quantity);
 console.log(typeof quantity);
 
-
 let quantity = prompt('enter the quantity of the produck');
 quantity = Number(quantity);
 console.log(quantity);
